206a8ce45b259bef2106752184a55a32bcd1655f
Niko <ighosta9@gmail.com>
2020-05-05 21:02:16 +0300

    News: - Init game - Shuffling - Checking
    (And some fixes)

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,35 +3,35 @@ import PropTypes from 'prop-types'
 
 class Cell extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     /*
     * For rename Cell's className it's needed to
     * call this func again, so we use: "...bind(this)".
     */
-    this.toggleClassName = this.toggleClassName.bind(this);
+    // this.toggleClassName = this.toggleClassName.bind(this)
 
     this.state = {
       isTapped: false,
-    };
+    }
   }
 
   // Func for flip card.
-  toggleClassName() {
-    this.setState(state => ({
-      isTapped: !state.isTapped
-    }));
-  }
+  // toggleClassName() {
+  //   this.setState(state => ({
+  //     isTapped: !state.isTapped,
+  //   }))
+  // }
 
   render () {
     // Importing this state into render() to className which has to read value of the state.
-    const { isTapped } = this.state;
+    const { isTapped } = this.state
 
     return (
       <div
         // There is our imported state.
-        className={`Field__сell${isTapped ? ' Field__cell_flipped' : ''}`}
+        className={'Field__сell' + (this.props.close ? '' : ' Field__cell_flipped') + (this.props.done ? ' Field__cell_done' : '')}
         onClick={() => {
-          this.toggleClassName()
+          // this.toggleClassName()
           this.props.onClick()
         }}
       >
@@ -45,6 +45,8 @@ class Cell extends Component {
 Cell.propTypes = {
   onClick: PropTypes.func,
   pair: PropTypes.number.isRequired,
+  close: PropTypes.bool.isRequired,
+  done: PropTypes.bool.isRequired,
 }
 
-export default Cell;
+export default Cell
--- src/components/Field.js
+++ src/components/Field.js
@@ -3,81 +3,313 @@ import Cell from './Cell'
 
 class Field extends Component {
   constructor(props) {
-    super(props);
+    super(props)
+
     this.state = {
-      cells: Array(16).fill(null),
-    };
+      cells: {
+        Init:     [...Array(8).keys()],
+        Dupe:     [],
+        Rand:     [],
+
+        Open:     [],
+
+        children: [],
+      }
+    }
+
+    this.initGame()
   }
+
+  handleClick = (indexArray, indexPair) => {
+    const
+      cells = this.state.cells,
+
+      currentCell = {
+        indexPair,
+        indexArray
+      }
+    
+    // TODO: Remove this condition
+    if (cells.Open.length === 2)
+      return null
+    else {
+      cells.children[indexArray].close = false
+      console.log(currentCell)
+      // TODO: Not same index array
+      cells.Open.push(currentCell)
+
+      console.log(cells.Open)
+
+      this.setState({
+        cells: {
+          ...this.state.cells,
+          children: cells.children,
+        }
+      })
+
+      if (cells.Open.length === 2)
+        setTimeout(() => this.check(), 1000)
+    }
+  }
+
+  check = () => {
+    const
+      cells = this.state.cells,
+      children = this.state.cells.children
+    
+    // TODO: Check out it on bugs...
+    if ((cells.Open[0].indexPair === cells.Open[1].indexPair) &&
+        (cells.Open[0].indexArray !== cells.Open[1].indexArray)) {
+      cells.children[cells.Open[0].indexArray].done = true
+      cells.children[cells.Open[1].indexArray].done = true
+    } else if (cells.Open[0].indexArray === cells.Open[1].indexArray) {
+      cells.children[cells.Open[0].indexArray].close = false
+      cells.children[cells.Open[1].indexArray].close = false
+    } else {
+      cells.children[cells.Open[0].indexArray].close = true
+      cells.children[cells.Open[1].indexArray].close = true
+    }
+
+    this.setState({
+      cells: {
+        ...this.state.cells,
+        Open: []
+      }
+    })
+  }
+
+  initGame = () => {
+    // Shortcut
+    const cells  = this.state.cells
+
+    let children = []
+
+    cells.Dupe   = cells.Init.concat(cells.Init) // Duplicating inited array (for pairs)
+    // cells.Rand   = this.shuffleCells(cells.Dupe) // Randomizing it by shuffleCells func
+    cells.Rand = cells.Dupe
+
+    // Putting default info in each child of already randomized cells
+    cells.Rand.map((pair, index) => {
+      return children.push({
+        pair,
+        close: true,
+        done: false,
+      })
+    })
+
+    cells.children = children
+  }
+
+  shuffleCells = (array) => {
+    let
+      currentIndex = array.length,
+      temporaryValue,
+      randomIndex
   
-
-  handleClick = (i, j) => {
-    console.log(i, j)
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex   = Math.floor(Math.random() * currentIndex)
+      currentIndex -= 1
+  
+      // And swap it with the current element.
+      temporaryValue      = array[currentIndex]
+      array[currentIndex] = array[randomIndex]
+      array[randomIndex]  = temporaryValue
+    }
+    return array
   }
 
-  renderCell = (indexPair, indexArr) => {
+  renderCell = (indexPair, indexArray, close, done) => {
     return (
       <Cell
         pair={indexPair}
-        key={indexArr}
-        onClick={() => {
-          this.handleClick(indexPair, indexArr);
-        }}
+        key={indexArray}
+        close={close}
+        done={done}
+        onClick={() => this.handleClick(indexArray, indexPair)}
       />
     )
   }
 
+  renderField = () => {
+    const
+      children   = this.state.cells.children, // Shortcut
+      lineLength = 4 // Count of cells in 1 line
+
+    let field = []
+
+    // Every line...
+    for (let i = 0; i < lineLength; i++) {
+      let rows = []
+
+      // Cells are created... 
+      for (let j = 0; j < children.length; j += lineLength) {
+        // In their own row
+        rows.push(<div className='Field__row' key={`Row_${j}`}>
+          {[ // In count of 4
+            this.renderCell(children[j].pair, j, children[j].close, children[j].done),
+            this.renderCell(children[j + 1].pair, j + 1, children[j + 1].close, children[j + 1].done),
+            this.renderCell(children[j + 2].pair, j + 2, children[j + 2].close, children[j + 2].done),
+            this.renderCell(children[j + 3].pair, j + 3, children[j + 3].close, children[j + 3].done),
+          ]}
+        </div>)
+      }
+      
+      // Then all line with their cells pushed into Field (parent) 
+      field.push(<div className='Field' key={`Field_${i}`}>{rows}</div>)
+
+      // Returned last updated and pushed 
+      return field
+    }
+  }
+
   render() {
-    return (
-      <div className='Field'>
-        <div className='Field__row'>
-          {this.renderCell(0, 0)}
-          {this.renderCell(0, 1)}
-          {this.renderCell(1, 2)}
-          {this.renderCell(1, 3)}
-        </div>
-        <div className='Field__row'>
-          {this.renderCell(2, 4)}
-          {this.renderCell(2, 5)}
-          {this.renderCell(3, 6)}
-          {this.renderCell(3, 7)}
-        </div>
-        <div className='Field__row'>
-          {this.renderCell(4, 8)}
-          {this.renderCell(4, 9)}
-          {this.renderCell(5, 10)}
-          {this.renderCell(5, 11)}
-        </div>
-        <div className='Field__row'>
-          {this.renderCell(6, 12)}
-          {this.renderCell(6, 13)}
-          {this.renderCell(7, 14)}
-          {this.renderCell(7, 15)}
-        </div>
-      </div>
-    )
+    return this.renderField()
   }
 }
 
+export default Field
 
-  // It's already prepared, in advance, function for use to shuffle cards when page loaded.
-  // shuffleCells = (array) => {
-  //   let
-  //     currentIndex = array.length,
-  //     temporaryValue,
-  //     randomIndex
-  
-  //   // While there remain elements to shuffle...
-  //   while (0 !== currentIndex) {
-  //     // Pick a remaining element...
-  //     randomIndex = Math.floor(Math.random() * currentIndex)
-  //     currentIndex -= 1
-  
-  //     // And swap it with the current element.
-  //     temporaryValue = array[currentIndex]
-  //     array[currentIndex] = array[randomIndex]
-  //     array[randomIndex] = temporaryValue;
-  //   }
-  //   return array
-  // };
+// handleClick = (indexArray, indexPair) => {
+//   const
+//     cells = this.state.cells,
 
-export default Field;
+//     currentCell = indexArray,
+//     children    = cells.children,
+//     open        = cells.Open
+
+//   if (open.length === 2)
+//     setTimeout(() => this.check(), 1000)
+//   else {
+//     children[indexArray].close = false;
+//     open.push(currentCell)
+
+//     this.setState({
+//       cells: {
+//         ...this.state.cells,
+//         Open: indexPair
+//       }
+//     })
+
+//     if (open.length === 2)
+//       setTimeout(() => this.check(), 1000)
+//   }
+
+//   console.log(cells)
+// }
+
+// check = () => {
+//   const
+//     cells       = this.state.cells,
+
+//     children    = cells.children,
+//     open        = cells.Open
+  
+//   if (open[0] === open[1]) {
+//     children[open[0]].done = true
+//     children[open[1]].done = true
+//   } else {
+//     children[open[0]].close = true
+//     children[open[1]].close = true
+//   }
+
+//   this.setState({
+//     cells: {
+//       ...this.state.cells,
+//       Open: []
+//     }
+//   })
+// }
+
+// initGame = () => {
+//   // Shortcut
+//   const cells  = this.state.cells
+
+//   let children = []
+
+//   cells.Dupe   = cells.Init.concat(cells.Init) // Duplicating inited array (for pairs)
+//   cells.Rand   = this.shuffleCells(cells.Dupe) // Randomizing it by shuffleCells func
+
+//   // Putting default info in each child of already randomized cells
+//   cells.Rand.map((pair, index) => {
+//     return children.push({
+//       pair,
+//       close: true,
+//       done: false,
+//     })
+//   })
+
+//   cells.children = children
+// }
+
+// shuffleCells = (array) => {
+//   let
+//     currentIndex = array.length,
+//     temporaryValue,
+//     randomIndex
+
+//   // While there remain elements to shuffle...
+//   while (0 !== currentIndex) {
+//     // Pick a remaining element...
+//     randomIndex   = Math.floor(Math.random() * currentIndex)
+//     currentIndex -= 1
+
+//     // And swap it with the current element.
+//     temporaryValue      = array[currentIndex]
+//     array[currentIndex] = array[randomIndex]
+//     array[randomIndex]  = temporaryValue
+//   }
+//   return array
+// }
+
+// renderCell = (indexPair, indexArray, close, done) => {
+//   return (
+//     <Cell
+//       pair={indexPair}
+//       key={indexArray}
+//       close={close}
+//       done={done}
+//       onClick={() => this.handleClick(indexArray, indexPair)}
+//     />
+//   )
+// }
+
+// renderField = () => {
+//   const
+//     children   = this.state.cells.children, // Shortcut
+//     lineLength = 4 // Count of cells in 1 line
+
+//   let field = []
+
+//   // Every line...
+//   for (let i = 0; i < lineLength; i++) {
+//     let rows = []
+
+//     // Cells are created... 
+//     for (let j = 0; j < children.length; j += lineLength) {
+//       // In their own row
+//       rows.push(<div className='Field__row' key={`Row_${j}`}>
+//         {[ // In count of 4
+//           this.renderCell(children[j].pair, j, children[j].close, children[j].done),
+//           this.renderCell(children[j + 1].pair, j + 1, children[j + 1].close, children[j + 1].done),
+//           this.renderCell(children[j + 2].pair, j + 2, children[j + 2].close, children[j + 2].done),
+//           this.renderCell(children[j + 3].pair, j + 3, children[j + 3].close, children[j + 3].done),
+//         ]}
+//       </div>)
+//     }
+    
+//     // Then all line with their cells pushed into Field (parent) 
+//     field.push(<div className='Field' key={`Field_${i}`}>{rows}</div>)
+
+//     // Returned last updated and pushed 
+//     return field
+//   }
+// }
+
+// render() {
+//   return this.renderField()
+// }
+// }
+
+// export default Field
