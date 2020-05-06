143555b1fb4a15af1f9a58ddd324bf9d89bd8596
Niko <ighosta9@gmail.com>
2020-05-06 23:00:33 +0300

    Updates

---
+++ precache-manifest.66de257711db71597453a84c244a9fe2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e1febd492d95a2112ece6e877cf78ca",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68da3337b99f5efa369a",
+    "url": "./static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "1c48a8dd5241431b8798",
+    "url": "./static/css/main.3f18c544.chunk.css"
+  },
+  {
+    "revision": "68da3337b99f5efa369a",
+    "url": "./static/js/2.6659fcdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.6659fcdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c48a8dd5241431b8798",
+    "url": "./static/js/main.4631135c.chunk.js"
+  },
+  {
+    "revision": "f60ca8c384daae52c311",
+    "url": "./static/js/runtime-main.f345cd69.js"
+  }
+]);
